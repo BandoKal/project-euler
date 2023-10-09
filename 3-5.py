3a92632ed647af1e9eb545f08acc1a384e2eccea
Jason Bandy <jason.bandy77@gmail.com>
2023-10-09 11:07:17 -0500

    problem 1

---
+++ 3-5.py
@@ -0,0 +1,11 @@
+# If we list all the natural numbers below that are multiples of or, we get and. The sum of these multiples is. Find the sum of all the multiples of or below.n = 1000
+a = 3
+b = 5
+sum = 0
+n = int(input("Enter a number: "))
+
+for i in range(n):
+    if i % a == 0 or i % b == 0:
+        sum += i
+
+print(sum)
